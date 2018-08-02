ee5fb1b670a1b8e7b4ac3d7b09a00deba1236f1b
Erick Zhang <erickzzh@gmail.com>
2018-08-02 15:25:43 -0400

    the blockchain is now immutable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import hashlib
 import datetime
+import json
+import pprint as pprint
 
 class Block:
     def __init__(self,index, timeStamp, data, previousBlock = ''):
@@ -11,7 +13,7 @@ class Block:
 
     
     def calculateHash(self, data, timeStamp):
-        data = data + str(timeStamp)
+        data = str(data) + str(timeStamp)
         data = data.encode()
         hash = hashlib.sha256(data)
         return hash.hexdigest()
@@ -22,7 +24,7 @@ class Blockchain:
         self.chain = [self.GenesisBlock()]
     
     def GenesisBlock(self):
-        genesisBlock = Block(0,datetime.datetime.now(),"Genesis Block","0")
+        genesisBlock = Block(0,str(datetime.datetime.now()),"Genesis Block","0")
         return genesisBlock
 
     def getLastBlock(self):
@@ -37,13 +39,38 @@ class Blockchain:
         '''this is obv not the best way to append a block but wth'''
         self.chain.append(newBlock)
 
+    def isChainValid(self):
+        for x in range(1,len(self.chain)):
+            currentBlock = self.chain[x]
+            previousBlock = self.chain[x-1]
+
+            if (currentBlock.previousBlock != previousBlock.hash):
+                return False
+            
+            if (currentBlock.hash != currentBlock.calculateHash(currentBlock.data,currentBlock.timeStamp)):
+                return False
+            
+        return True
+
+
+    
+
+
+
 ezmoney = Blockchain()
-hey =  Block(1,"1","1")
-what =  Block(1,"1","1")
+hey =  Block(1,"1",{"amount":5})
+what =  Block(1,"1",{"amount": 69})
 ezmoney.appendBlock(hey)
 ezmoney.appendBlock(what)
+"""this is to convert the blockchain into a json format"""
+testChain = []
+for x in ezmoney.chain:
+    temp = json.dumps(x.__dict__)
+    testChain.append(temp)
 
 
+pprint.pprint(testChain)
+print(ezmoney.isChainValid())
   
 
 
